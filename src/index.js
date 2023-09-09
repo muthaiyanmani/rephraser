8c96ac5028dc302ee6ae1823d51d80c9307abdb5
Muthaiyan Mani <muthaiyan.mb@zohocorp.com>
2023-09-09 14:41:21 +0530

    chore: initialized git

---
+++ src/index.js
@@ -0,0 +1,24 @@
+let changeColor = document.getElementById("changeColor");
+
+
+changeColor.addEventListener("click", async() => {
+    let inputtag = document.querySelector("#tagcolor");
+    chrome.storage.sync.set({ inputtag: inputtag.value });
+ 
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+ 
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: setBorderColor,
+    });
+ });
+ 
+ function setBorderColor() {
+    chrome.storage.sync.get("inputtag", ({ inputtag }) => {
+      document.querySelectorAll(inputtag).forEach((element) => {
+        element.style.border = "1px solid red";
+      });
+    });
+ }
+
+ chrome.action.setIcon({ path: "/public/assets/logo-success-48.png" });
