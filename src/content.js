123ddb07bc1b53af9e2234a5666c4ad0f4a1ac88
Muthaiyan Mani <muthaiyan.mb@zohocorp.com>
2023-09-09 19:35:35 +0530

    fix:  dynamicinput event listening using MutationObserver

--- src/content.js
+++ src/content.js
@@ -1,11 +1,9 @@
-
 const DEBOUNCE_DELAY = 800;
 
-let apiKey = '';
+let apiKey = "";
+const processedElements = new Set();
 
-chrome.storage.local.get(["apiToken"], function (items) {
-  apiKey = items?.apiToken;
-});
+chrome.storage.local.get(["apiToken"], function (items) {apiKey = items?.apiToken});
 
 function debounce(func, delay) {
   let timerId;
@@ -19,63 +17,83 @@ function debounce(func, delay) {
 
 async function handleInputChange(event) {
   const inputElement = event.target;
-  const inputValue = event.target.value;
-  const resp = await processMessage(inputValue);
-  const chatBubble = createChatBubble(resp);
-  console.log(chatBubble);
-  inputElement.insertAdjacentElement("afterend", chatBubble);
+  const inputValue = event?.target?.value || event?.target?.innerText;
+
+  if (inputValue) {
+    const resp = await processMessage(inputValue);
+    removeChatBubble();
+    const chatBubble = createChatBubble(resp);
+    inputElement.insertAdjacentElement("afterend", chatBubble);
+  }
 }
 
-function processInputElements() {
-  const textInputs = document.querySelectorAll('input[type="text"], textarea');
-
-  textInputs.forEach((element) => {
-    element.addEventListener("input", debounce(handleInputChange,DEBOUNCE_DELAY));
-  });
+function handleMutation(mutationsList, observer) {
+  for (const mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      const textInputs = document.querySelectorAll('[data-purpose="zcslash"], input[type="text"], textarea');
+      
+      const filteredInputs = Array.from(textInputs).filter((element) => !processedElements.has(element));
+      filteredInputs.forEach((element) => {
+        processedElements.add(element);
+        element.addEventListener("input",debounce(handleInputChange, DEBOUNCE_DELAY));
+      });
+    }
+  }
 }
 
-async function processMessage(message='') {
+async function processMessage(message = "") {
   try {
-
     // TODO: Add error handling
     if (!apiKey) return;
 
-    const apiUrl = 'https://api.openai.com/v1/completions';
+    const apiUrl = "https://api.openai.com/v1/completions";
     const resp = await fetch(apiUrl, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`
       },
       body: JSON.stringify({
         prompt: `Please rephrase and correct the following sentence: "${message}"`,
         max_tokens: 50,
-        model: 'text-davinci-003',
-      }),
+        model: "text-davinci-003"
+      })
     });
     const data = await resp.json();
     return data.choices[0].text;
-  }catch(error) {
+  } catch (error) {
     console.error(error);
   }
 }
 
 function createChatBubble(text) {
-  const bubble = document.createElement('div');
-  bubble.classList.add('chat-bubble');
+  const bubble = document.createElement("div");
+  bubble.setAttribute("data-name", "rephrase");
+  bubble.classList.add("chat-bubble");
 
-  bubble.style.position = 'absolute';
-  bubble.style.top = '0';
-  bubble.style.right = '0';
-  bubble.style.backgroundColor = 'red';
-  bubble.style.color = 'white';
-  bubble.style.padding = '12px';
-  bubble.style.borderRadius = '5px';
-  bubble.style.zIndex = '9999';
-  const chat = bubble.appendChild(document.createElement('p'));
-  chat.style.cursor = 'pointer';
+  bubble.style.position = "absolute";
+  bubble.style.top = "0";
+  bubble.style.right = "0";
+  bubble.style.backgroundColor = "red";
+  bubble.style.color = "white";
+  bubble.style.padding = "8px";
+  bubble.style.borderRadius = "5px";
+  bubble.style.zIndex = "9999";
+  bubble.style.fontSize = "11px";
+  bubble.style.width = "10%";
+  const chat = bubble.appendChild(document.createElement("p"));
+  chat.style.cursor = "pointer";
   chat.textContent = text;
   return bubble;
 }
 
-processInputElements();
+const removeChatBubble = (event) => {
+  const chatBubble = document.querySelector('[data-name="rephrase"]');
+  if (chatBubble) {
+    chatBubble.remove();
+  }
+};
+
+// Start observing changes in the DOM
+const observer = new MutationObserver(handleMutation);
+observer.observe(document.body, { childList: true,subtree: true });
